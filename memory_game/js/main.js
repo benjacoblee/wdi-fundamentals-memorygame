0ad887c7b5d389ae196dc3925bf1be8d46d8bf08
benjacoblee <benjacoblee@gmail.com>
2019-08-20 13:37:44 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,44 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [ {
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+}, 
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+}, 
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+}, 
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
-function checkForMatch () {
+var checkForMatch = function() {
 if (cardsInPlay[0] === cardsInPlay[1])
   	alert("You found a match!");
 else 
   	alert("Sorry, try again.")
 }
 
-function flipCard (cardId) {
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+var flipCard = function(cardID) {
+console.log("User flipped " + cards[cardID].rank);
+console.log(cards[cardID].cardImage);
+console.log(cards[cardID].suit);
+cardsInPlay.push(cards[cardID].rank);
 checkForMatch ()
 }
 flipCard(0);
 flipCard(2);
+
 
 /*if (cardsInPlay.length === 2) {
 	console.log("You have selected two cards"); */
