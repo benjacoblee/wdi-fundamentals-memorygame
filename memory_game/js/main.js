cd296dc43288c2c2e2b9b81be04f485295f99666
benn <benjacoblee@gmail.com>
2019-09-09 18:55:27 +0800

    retry

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,7 @@
-var cards = ['queen', 'queen', 'king', 'king'];
-var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
 
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert('You found a match!')
-} else {
-	alert('Sorry, try again')
-}
-
-console.log('User flipped ' + cardOne)
-console.log('User flipped ' + cardTwo)
-
-
+console.log(`User flipped ${cardOne}`)
+console.log(`User flipped ${cardThree}`)
