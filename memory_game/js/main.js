138cdbc4da82fc2ecaf6094a363ff332e684e7c4
benn <benjacoblee@gmail.com>
2019-09-09 17:57:47 +0800

    starting from scratch

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,50 +1,6 @@
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king'
 
-var cards = [ {
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-}, 
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-}, 
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
-}, 
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
-}
-];
-var cardsInPlay = [];
-
-var checkForMatch = function() {
-if (cardsInPlay[0] === cardsInPlay[1])
-  	alert("You found a match!");
-else 
-  	alert("Sorry, try again.")
-}
-
-var flipCard = function(cardID) {
-console.log("User flipped " + cards[cardID].rank);
-console.log(cards[cardID].cardImage);
-console.log(cards[cardID].suit);
-cardsInPlay.push(cards[cardID].rank);
-checkForMatch ()
-}
-flipCard(0);
-flipCard(2);
-
-function createBoard () {
-for (var i = 0; i < /*array name*/; i++);
-	
-};
-var cardElement = document.createElement('img');
-
-/*if (cardsInPlay.length === 2) {
-	console.log("You have selected two cards"); */
-
+console.log('User flipped ' + cardOne)
